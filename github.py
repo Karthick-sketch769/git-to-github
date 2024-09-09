4b129dbe81d6104f3ea2e1b3f0569cbc793bfa83
root <root@ip-172-31-39-229.ap-southeast-2.compute.internal>
2024-09-09 19:00:12 +0000

    hello world to team

--- github.py
+++ github.py
@@ -4,6 +4,6 @@ public class HelloWorld {
 
     // The main method is the entry point of any Java application
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        // Print "Hello, team!" to the console
+        System.out.println("Hello, team!");
     }
