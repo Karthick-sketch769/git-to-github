7d8061638741b198ab5febd96c1b234711d3f5e9
Karthick-sketch769 <karthicksethu189@gmail.com>
2024-09-10 02:08:41 +0530

    Update github.py

--- github.py
+++ github.py
@@ -1,11 +1,6 @@
-
 // This is a simple Java program that prints "Hello,world!" to the console
-=======
 // This is a simple Java program that prints "Hello, team!" to the console
-
-
 public class Hello team {
-
     // The main method is the entry point of any Java application
     public static void main(String[] args) {
         // Print "Hello, world!" to the console
