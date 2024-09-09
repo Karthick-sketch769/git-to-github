8fc82819eac62e34fe7b783db17f8850520ea11e
root <root@ip-172-31-39-229.ap-southeast-2.compute.internal>
2024-09-09 19:37:25 +0000

    chnged to master

--- github.py
+++ github.py
@@ -1,9 +1,9 @@
-// This is a simple Java program that prints "Hello, World!" to the console
+// This is a simple Java program that prints "Hello,world!" to the console
 
 public class HelloWorld {
 
     // The main method is the entry point of any Java application
     public static void main(String[] args) {
-        // Print "Hello, team!" to the console
-        System.out.println("Hello, team!");
+        // Print "Hello, world!" to the console
+        System.out.println("Hello,world!");
     }
